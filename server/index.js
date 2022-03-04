a9171a6d48745b06fe6c09c7edbccadeb80ececd
Laura Horsley <horsleyshorses02@gmail.com>
2022-03-04 11:51:00 -0800

    Incomplete Commit

--- server/index.js
+++ server/index.js
@@ -1,25 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 
+const {getCompliment, getFortune, createDessert, updateDessert, deleteDessert} = require('./controller')
+
 const app = express();
 
 
+app.use(express.json()); // When we want to be able to accept JSON.
 app.use(cors());
 
-app.use(express.json()); // When we want to be able to accept JSON.
 
-app.get("/api/compliment", (req, res) => {
-  const compliments = ["Gee, you're a smart cookie!",
-					 "Cool shirt!",
-					 "Your Javascript skills are stellar.",
-  ];
 
-  // choose random compliment
-  let randomIndex = Math.floor(Math.random() * compliments.length);
-  let randomCompliment = compliments[randomIndex];
-
-  res.status(200).send(randomCompliment);
-  
-});
+app.get('/api/compliment', getCompliment);
+app.get('/api/fortune', getFortune);
+// app.post('/api/dessert', createDessert);
+// app.delete('/api/dessert/:id', deleteDessert)
+// app.put('/api/dessert/:id', updateDessert)
 
 app.listen(4000, () => console.log("Server running on 4000"));
